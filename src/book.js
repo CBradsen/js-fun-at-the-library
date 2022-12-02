5d8a16539f30910fecd5b87f85d7e83dad84648a
Carol Bradsen <carolbradsen@gmail.com>
2022-12-01 17:19:57 -0700

    Add createTitle passed tests

--- src/book.js
+++ src/book.js
@@ -1,6 +1,8 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  bookIdea = `The ${bookIdea}`;
+  return bookIdea;
 }
+createTitle("Storm's Awakening");
 
 module.exports = {
   createTitle,
