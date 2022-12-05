7bd9c2a86834870152f1094721b49d97414c7ca7
Carol Bradsen <carolbradsen@gmail.com>
2022-12-04 19:08:38 -0700

    Add 2 tests from Library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,49 @@
+function createLibrary(libraryName) {
+  var libraryName = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [], 
+      nonFiction: []
+    }
+  }
+return libraryName
+}
+
+function addBook(libraryName, book) {
+libraryName.shelves[book.genre].push(book);
+
+}
+function checkoutBook(library, book, genreShelf) {
+  //needs to find which genreshelf the book is on.
+  //using: fantasy, fiction or nonFiction
+//var checkedOut = [];
+  for (var i = 0; i < genreShelf.length; i++) {
+    if (genreShelf.title === book) {
+        checkedOut.push(book);
+    }
+   // console.log(checkedOut);
+  }
+
+}
+/*function unshelfBook(bookName, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+   if (shelf[i].title === bookName) {
+   shelf.splice(i, 1);
+  }
+  }
+}
+function unshelfBook(bookName, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+   if (shelf[i].title === bookName) {
+   shelf.splice(i, 1);
+  }
+  }
+} */
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
